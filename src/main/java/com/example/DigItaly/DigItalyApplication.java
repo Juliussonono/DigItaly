f71edc0f38349fb9d0b440678d7651eb08d4d20b
juliussonono <sonono26@gmail.com>
2022-09-06 15:31:49 +0200

    Initial commit

---
+++ src/main/java/com/example/DigItaly/DigItalyApplication.java
@@ -0,0 +1,13 @@
+package com.example.DigItaly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigItalyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigItalyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DigItaly/DigItalyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DigItaly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigItalyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

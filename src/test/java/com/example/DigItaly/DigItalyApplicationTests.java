94c5189d7d2fd1bc610429d7cc0119f0978b77df
SipanIsik <sipanisik90@gmail.com>
2022-09-09 11:32:02 +0200

    G-unet-test

--- src/test/java/com/example/DigItaly/DigItalyApplicationTests.java
+++ src/test/java/com/example/DigItaly/DigItalyApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.DigItaly;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DigItalyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Test
+    void contextLoads() {
+    }
+    @Test
+    public void testGetProduct() throws Exception {
+        mvc.perform(
+                        MockMvcRequestBuilders.get("/")
+                )
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.content().string(containsString("Parmigiano Reggiano")));
+
+    }
 
 }

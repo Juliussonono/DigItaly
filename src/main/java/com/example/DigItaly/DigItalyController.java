18ef3cf138a1ee7094ad5bb7c3a65fedd93108e2
Skeppar <alvin.skeppar@gmail.com>
2022-09-22 11:29:59 +0200

    Cart now working

--- src/main/java/com/example/DigItaly/DigItalyController.java
+++ src/main/java/com/example/DigItaly/DigItalyController.java
@@ -3,13 +3,11 @@ package com.example.DigItaly;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class DigItalyController {
@@ -26,9 +24,47 @@ public class DigItalyController {
     @Autowired
     ProfileCustomerRepository customerRepository;
 
-    @GetMapping ("/cart")
-    public String cart() {
+/*
+    @GetMapping ("/cart/{id}")
+    public String cart(Model model, @PathVariable Integer id, HttpSession session) {
+        // Now it resets the cart each time, have to add if a cart in the session exists. If null make new, otherwise add to existing.
+        Product product = productRepository.findById(id).get();
+        model.addAttribute(product);
+        List<Product> cart = new ArrayList<Product>();
+        cart.add(product);
+        session.setAttribute("cart", cart);
         return "cart";
+    }
+
+ */
+
+    @PostMapping ("/cart/{id}")
+    public String cart(Model model, @PathVariable Integer id, HttpSession session) {
+        Product product = productRepository.findById(id).get();
+        if (session.getAttribute("cart") == null) {
+            List<ItemQuantity> cart = new ArrayList<ItemQuantity>();
+            cart.add(new ItemQuantity(product, 1));
+            session.setAttribute("cart", cart);
+        } else {
+            List<ItemQuantity> cart = (List<ItemQuantity>) session.getAttribute("cart");
+            int index = this.exists(id, cart);
+            if (index == -1) {
+                cart.add(new ItemQuantity(product, 1));
+            } else {
+                int quantity = cart.get(index).getQuantity() + 1;
+                cart.get(index).setQuantity(quantity);
+            }
+            session.setAttribute("cart", cart);
+        } return "cart";
+    }
+
+    private int exists(Integer id, List<ItemQuantity> cart) {
+        for (int i = 0; i < cart.size(); i++) {
+            if (cart.get(i).getProduct().getId().equals(id)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @GetMapping ("/login")
--- src/main/java/com/example/DigItaly/Product.java
+++ src/main/java/com/example/DigItaly/Product.java
@@ -11,10 +11,7 @@ public class Product {
     private String name;
     private String brand;
     private Double price;
-
-
     private String image;
-
 
 
     public Product() {}
